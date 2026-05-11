19b7d5c12718988054d8ce0fb9a77ecf442ac116
thanush2412 <thanushkrishna13@gmail.com>
2026-05-11 15:44:40 +0530

    LIFO

--- components/candidates-table.tsx
+++ components/candidates-table.tsx
@@ -180,7 +180,7 @@ export default function CandidatesTable({ sheetUrl, columnMapping = {} }: { shee
       const json: SheetResponse = await res.json();
       if (json.status === "success") {
         setHeaders(json.headers);
-        setAllRows(json.data);
+        setAllRows([...json.data].reverse());
         setPage(1);
         setSelected(new Set());
         if (!colsInitialized) {
